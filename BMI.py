82fdd97f0e74abcb520cd1947a4e57301c2bba31
DariaMajc <daria.majchrowska@wp.pl>
2022-02-23 13:32:04 +0100

    my first repostiory

---
+++ BMI.py
@@ -0,0 +1,11 @@
+moja_masa = input('Jaka jest Twoja masa?')
+moja_masa = int(moja_masa)
+moj_wzrost = input('Jaki jest Twoj wzrost?')
+moj_wzrost = float(moj_wzrost)
+
+BMI = moja_masa / (moj_wzrost**2)
+
+print(BMI)
+
+
+
---
+++ weeks.py
@@ -0,0 +1,6 @@
+tygodnie = 7
+minuty = 60
+godziny = 24
+dni = 7
+suma = minuty * godziny * dni * tygodnie
+print("liczba minut w ciagu siedmiu tygodni to ", suma)
